2f632d1a518b8cbb22ec3b57ea7676c0771230b9
Meg Fehres <34945758+mhfehres@users.noreply.github.com>
2023-03-05 16:42:56 -0700

    Added preprocessor and data.

---
+++ Preprocess.py
@@ -0,0 +1,19 @@
+import networkx as nx
+import csv
+
+def largest_connected_component(filename):
+    # Read the CSV file
+    with open(filename) as csvfile:
+        reader = csv.reader(csvfile)
+        edges = [(row[0], row[1]) for row in reader]
+
+    # Create a graph and add the edges
+    G = nx.Graph()
+    G.add_edges_from(edges)
+
+    # Find the largest connected component
+    largest_cc = max(nx.connected_components(G), key=len)
+
+    # Return the largest connected component
+    return largest_cc
+
